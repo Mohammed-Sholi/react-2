51b4c1e612828d1825e8616851602acaef28efc0
unknown <programmer3539@gmail.com>
2024-02-19 21:54:47 -0800

    react-2

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  function x () {
+    setCount(count + 1);
+  }
+  return (
+    <>
+      <button onClick={x}>count is {count}</button>
+    </>
+  )
+}
+
+export default App
